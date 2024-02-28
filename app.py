803b9a962ce08485f4cf7be89bf49112fe55465a
miridium-electrode <checkerdance@yahoo.com>
2024-02-28 12:55:38 +0700

    backup

--- app.py
+++ app.py
@@ -19,50 +19,105 @@ nvidia = pd.read_csv('nvda/price_data.csv')
 def calculate_historical_return(starting_price: float, ending_price: float):
     return (ending_price - starting_price) / starting_price
 
-def final_investment_value(initial_investent: float|int, historical_return: float):
-    return initial_investent * (1 + historical_return)
+def final_investment_value(initial_investment: float|int, historical_return: float):
+    return initial_investment * (1 + historical_return)
 
 ###
 # streamlit code start here
 ###
 
+# apa itu historical return
+# formula kalkulasi
+# contoh perusahaan dan info
+# info singkat return dalam bentuk grafik
+# grafik historical return
+# contoh nilai kembalian untuk 5 tahun
+# kesimpulan
+
 # Header
-st.header("Perbandingan *historical return* Saham Perusahaan dan Index")
+st.header("Kalkulasi *Historical Return* Pada Saham Perusahaan")
 
 
-# Pendahuluan
-st.subheader("Pendahuluan")
-
-"""
-- *Historical return* adalah performa masa lampau dari sekuritas/index.
-- *Historical return* digunakan sebagai salah satu data yang digunakan analis dan investor untuk 
-memperkirakan return sekuritas/index pada masa depan. 
-- Investor dapat memperkirakan *historical return*  dari investasi apapun, seperti: 
-    - sekuritas
-    - rumah
-    - real estate
-    - reksa dana
-    - etf
-    - komoditasi
-      - emas
-      - bahan pangan
-      - minyak
-      - listrik
-      - air
-"""
-
-# Saham dan Index
-st.subheader("Saham")
+# Apa itu Historical Return
+st.subheader("Apa itu *Historical Return*")
 
 """
-Berikut adalah Saham yang dipilih untuk dianalisa *historical return*-nya
-- apple
-- amazon
-- google
-- meta
-- nvidia
-- microsoft
+*Historical return* adalah nilai yang dapat digunakan untuk mengukur performa dari sebuah investasi. 
+Analis/investor menggunakan *historical return* untuk mencoba memprediksi *return* di masa depan 
+atau memperkirakan bagaimana produk investasi akan bereaksi pada suatu kejadian. Investasi 
+yang dapat dikalkulasikan *historical return*-nya:
+- Saham
+- Reksa Dana
+- ETF
+- Index
+- Komoditas
+
+Namun, investor perlu berhati hati dalam menggunakan *historical return* 
+karena nilai pada masa lampau belum tentu dapat memprediksi masa depan bahkan
+jika kondisi yang sama terulang. Walaupun begitu, informasi ini adalah salah
+informasi yang berguna sebagai salah satu komponen dari analisa komprehensif investor/analis
 """
+
+# Rumus Historical Return
+st.subheader("Rumus *Historical Return*")
+
+"""
+*Historical return* didapatkan dari kalkulasi data dari masa lampau.
+untuk kalkulasi *historical return* dapat menggunakan rumus ini
+
+$$
+\\text{historical return} = \\frac{\\text{harga investasi akhir} - \\text{harga investasi awal}}{\\text{harga saham awal}}
+$$
+
+harga saham akhir adalah harga investasi pada periode waktu akhir yang dipilih dan 
+harga saham awal adalah harga saham yang dipilih pada periode waktu awal. Harga saham yang dipilih
+adalah harga *adjusted close* yaitu harga saham yang sudah dikalkulasikan berdasarkan *stock split*
+dan dividen
+"""
+
+# Data Perusahaan
+st.subheader("Data Perusahaan")
+
+
+"""
+Ada 6 perusahaan teknologi yang dipilih untuk pencarian nilai *historical return*
+Info singkat dari perusahaan:
+- **Apple Inc.**
+
+  perusahaan teknologi multinasional amerika yang menjual alat elektronik konsumen dan 
+  perangkat lunak seperti iPhone, iPad, Mac, Apple Watch, Apple TV, iTunes, iCloud, Apple Music 
+
+- **Amazon**
+
+  perusahaan multinasiona amerika dengan produk e-commerce, komputasi awan,
+  streaming konten digital dan kecerdasan buatan
+
+- **Google**
+  
+  perusahaan multinasional amerika yang bergeriak di bidang mesin pencari,
+  kecerdasan buatan, iklan online, perangkat lunak, komputasi awan
+
+- **Meta**
+
+  perusahaan multinasional amerika yang mengoperasikan 
+  whatsapp, facebook, instagram. Meta juga bergerak di virtual reality
+  setelah membeli perusahaan Oculus (sekarang reality labs)
+
+- **Nvidia**
+
+  perusahaan multinasional amerika yang bergerak di bidang GPU, antaruka
+  program untuk *data science* dan *High performance computing* dan supplier
+  hardware dan software kecerdasan buatan
+
+- **Microsoft**
+
+  perusahaan multinasional amerika yang terkenal dengan sistem operasi windows dan
+  perangkat lunak sepert Microsoft 365 dan edge browser
+
+"""
+
+# Analisa
+st.subheader("Analisa")
 
 closing_prices = pd.DataFrame({
     'date': apple['date'],
@@ -83,52 +138,118 @@ melted = melted.rename(
         'variable': 'Stocks'
     })
 
+
 # https://altair-viz.github.io/gallery/line_chart_with_custom_legend.html
 adjusted_close_chart = alt.Chart(melted).mark_line().encode(
-    x='Date',
-    y='Adjusted Close',
-    color='Stocks'
+    x="Date",
+    y="Adjusted Close",
+    color=alt.Color('Stocks')
 ).properties(
-    width=704,
+    width=704
 )
 
 adjusted_close_chart = adjusted_close_chart.configure_axisX(labelAngle=0)
 
+"""
+berikut adalah data saham harga jual 6 perusahaan
+"""
+
 st.altair_chart(adjusted_close_chart)
 
-roi_apple = calculate_historical_return(closing_prices['apple'].iloc[0], closing_prices['apple'].iloc[-1])
-roi_amazon = calculate_historical_return(closing_prices['amazon'].iloc[0], closing_prices['amazon'].iloc[-1])
-roi_google = calculate_historical_return(closing_prices['google'].iloc[0], closing_prices['google'].iloc[-1])
-roi_meta = calculate_historical_return(closing_prices['meta'].iloc[0], closing_prices['meta'].iloc[-1])
-roi_nvidia = calculate_historical_return(closing_prices['nvidia'].iloc[0], closing_prices['nvidia'].iloc[-1])
-roi_microsoft = calculate_historical_return(closing_prices['microsoft'].iloc[0], closing_prices['microsoft'].iloc[-1])
+historical_return_apple = calculate_historical_return(closing_prices['apple'].iloc[0], closing_prices['apple'].iloc[-1])
+historical_return_amazon = calculate_historical_return(closing_prices['amazon'].iloc[0], closing_prices['amazon'].iloc[-1])
+historical_return_google = calculate_historical_return(closing_prices['google'].iloc[0], closing_prices['google'].iloc[-1])
+historical_return_meta = calculate_historical_return(closing_prices['meta'].iloc[0], closing_prices['meta'].iloc[-1])
+historical_return_nvidia = calculate_historical_return(closing_prices['nvidia'].iloc[0], closing_prices['nvidia'].iloc[-1])
+historical_return_microsoft = calculate_historical_return(closing_prices['microsoft'].iloc[0], closing_prices['microsoft'].iloc[-1])
 
-roi_raw_table = [
-    ('apple', roi_apple),
-    ('amazon', roi_amazon),
-    ('google', roi_google),
-    ('meta', roi_meta),
-    ('nvidia', roi_nvidia),
-    ('microsoft', roi_microsoft)
+hist_ret_raw_table = [
+    ('apple', historical_return_apple),
+    ('amazon', historical_return_amazon),
+    ('google', historical_return_google),
+    ('meta', historical_return_meta),
+    ('nvidia', historical_return_nvidia),
+    ('microsoft', historical_return_microsoft)
 ]
 
-roi_dataframe = pd.DataFrame(data=roi_raw_table, columns=['stock', 'roi'])
+hist_ret_dataframe = pd.DataFrame(data=hist_ret_raw_table, columns=['Stock', 'Historical Return'])
 
-st.bar_chart(data=roi_dataframe, x='stock', y='roi')
+hist_ret_bar = alt.Chart(hist_ret_dataframe).mark_bar().encode(
+    x='Stock',
+    y='Historical Return',
+    color=alt.Color("Stock")
+).properties(
+    width=704
+)
 
-# Analisa
-st.subheader("Analisa")
+hist_ret_text = hist_ret_bar.mark_text(
+    align="center",
+    dy=-5
+).encode(text="Historical Return")
+
+hist_res_chart = hist_ret_text + hist_ret_bar
+
+hist_res_chart = hist_res_chart.configure_axisX(labelAngle=0)
+
+hist_res_chart = hist_res_chart.configure_range(
+    category={'scheme': 'dark2'}
+)
+
+"""
+berikut adalah data return historical 6 perusahaan
+"""
+
+st.altair_chart(hist_res_chart)
 
 initial_investment = st.number_input('Nilai investasi awal', value=1000)
 
-final_investment_apple = final_investment_value(initial_investment, roi_apple)
+final_investment_apple = final_investment_value(initial_investment, historical_return_apple)
 numerized_apple = numerize(final_investment_apple)
 
-st.metric(label='Apple', value=numerized_apple)
+final_investment_amazon = final_investment_value(initial_investment, historical_return_amazon)
+numerized_amazon = numerize(final_investment_amazon)
+
+final_investment_google = final_investment_value(initial_investment, historical_return_google)
+numerized_google = numerize(final_investment_google)
+
+final_investment_meta = final_investment_value(initial_investment, historical_return_meta)
+numerized_meta = numerize(final_investment_meta)
+
+final_investment_nvidia = final_investment_value(initial_investment, historical_return_nvidia)
+numerized_nvidia = numerize(final_investment_nvidia)
+
+final_investment_microsoft = final_investment_value(initial_investment, historical_return_microsoft)
+numerized_microsoft = numerize(final_investment_microsoft)
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.metric(label='Apple', value=numerized_apple)
+
+with col2:
+    st.metric(label='Amazon', value=numerized_amazon)
+
+with col3:
+    st.metric(label='Google', value=numerized_google)
+
+
+col4, col5, col6 = st.columns(3)
+
+with col4:
+    st.metric(label='Meta', value=numerized_meta)
+
+with col5:
+    st.metric(label='Nvidia', value=numerized_nvidia)
+
+with col6:
+    st.metric(label='Microsoft', value=numerized_microsoft)
 
 # Kesimpulan
 st.subheader("Kesimpulan")
 
+"""
+sebagai salah satu informasi yang dapat digunakan untuk menganalisa
+performa perusahaan di masa depan
 
-
-
+analisa ini menunjukkan pola yang pada saham perusahaan
+"""
---
+++ eng draft.md
@@ -0,0 +1,54 @@
+historical return is a metric for measuring performance of an investment. Investor and analyst could used
+historical metric to as one of data point to predict
+the performance of an investment or to figure out how 
+an investment would react when certain event happen. Here are list
+of invesment that could be measured it's historical performance:
+- stock
+- index
+- real estate
+- etf
+
+however caution should be exercised as past data won't predict
+what will happen in the future, even if certain event repeat. That being said, it's a valuable information for investor and analyst as part of their overall analysis.
+
+---
+
+to calculate the historical performance, one can use this formula:
+
+historical performance = (ending value - beginning value)/beginning value
+
+the ending value is the price of investment at the chosen ending period and the beginning value is the value of investment at the ending period
+
+---
+
+
+to demonstrate it, 6 company is chosen to calculate and compare it's historical value. there are ?? reason that 6 of this company is chosen
+- it's tech company, the company that are going through interesting time nowaday with ai craze. This 6 company all used ai one way or another
+- to not confuse the reader as to many graph will be overbearing
+- the data is fetched from alpha vantage free tier API, the company price data is available there and alpha vantage free tier has limitation
+
+with that being said here is a quick info of the company
+
+skipped
+
+---
+
+to start with the historical return let's display a graph of company adjusted return over period of 5 year. 
+
+the x axis display the date of the data and the y axis display the adjusted closing price. That is the stock closing price after taking account of stock split and dividend. From this data, Company have steady increase over time except nvidia.
+
+Next, we compare the historical return of the company with this bar chart
+
+the x axis display the name of the company and the y axis display numerical value of the historical return. It seems nvidia is the biggest considering the adjusted closing price that have drastic increase
+
+to use this historical return. let's say we will invest 1000 us$ at the starting period the final investment value you will get assumming you don't invest again and not accounting tax and inflation will be calculated using this formula:
+
+final investment value = initial investment value * (1 + historical return)
+
+here's an interactive form you can use to calculate final investment value:
+
+...
+
+---
+
+Historical value, is a quick information investor/analyst could get to know more about investment they want to invest. As flawed as it seems one more known information could be used as part of comprehensive financial analysis or as a filter to decide whether an investor/analyst want to know more about an investment and decide.
